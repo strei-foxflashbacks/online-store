61cb49482ee3eff3ffbcd19f0d81148192097708
ssekridov@gmail.com <ssekridov@gmail.com>
2022-12-15 19:30:50 +0100

    feat: product database fill

--- src/components/product-data.ts
+++ src/components/product-data.ts
@@ -2,291 +2,631 @@ import { IProductData } from '../types/IProductData'
 
 export const productData: IProductData[] = [
   {
-    name: 'Item 1',
+    name: 'Women\'s Institute Soft Blue Premium Acrylic Yarn 100g',
     category: 'Yarn',
-    brand: 'Macrametr',
-    description: '',
-    price: 1,
-    stock: 1,
-    photo: '',
+    brand: 'Women\'s Institute',
+    description: `
+    Ball Weight
+      100 Grams
+    Blend
+      100% Acrylic
+    Craft
+      Knitting
+    Crochet Hook Size
+      4.00mm (F)
+    Needle Size
+      4.00mm (US 6)
+    Tension
+      23 stitches; 30 rows to 10cm/4in
+    Collection
+      WI Premium Acrylic
+    Knitting Needle Width
+      4.00mm
+    Yarn Length
+      250m
+    Yarn Weight
+      DK `,
+    price: 2.50,
+    stock: 1256,
+    photo: './assets/1.jpg',
   },
   {
-    name: 'Item 2',
+    name: 'Women\'s Institute Light Pink Soft and Cuddly DK Yarn 50g',
     category: 'Yarn',
-    brand: 'Macrametr',
-    description: '',
-    price: 1,
-    stock: 1,
-    photo: '',
+    brand: 'Women\'s Institute',
+    description: `
+    Ball Weight
+      50 Grams
+    Blend
+      100% Acrylic
+    Craft
+      Knitting
+    Crochet Hook Size
+      4.00mm (F)
+    Needle Size
+      4.00mm (US 6)
+    Tension
+      25 stitches; 28 rows to 10cm/4in
+    Collection
+      WI Soft And Cuddly
+    Knitting Needle Width
+      4.00mm
+    Yarn Length
+      125m
+    Yarn Weight
+      DK `,
+    price: 4.50,
+    stock: 2454,
+    photo: './assets/2.jpg',
   },
   {
-    name: 'Item 3',
+    name: 'Women\'s Institute Grey Mix Soft and Cuddly DK Yarn 50g',
     category: 'Yarn',
-    brand: 'Macrametr',
-    description: '',
-    price: 1,
-    stock: 1,
-    photo: '',
+    brand: 'Women\'s Institute',
+    description: `
+    Ball Weight
+      50 Grams
+    Blend
+      100% Acrylic
+    Craft
+      Knitting
+    Crochet Hook Size
+      4.00mm (F)
+    Needle Size
+      4.00mm (US 6)
+    Tension
+      28 rows, 25 stitches to 10cm/4in
+    Collection
+      WI Soft And Cuddly
+    Yarn Length
+      125m
+    Yarn Weight
+      DK `,
+    price: 4.50,
+    stock: 569,
+    photo: './assets/3.jpg',
   },
   {
-    name: 'Item 4',
+    name: 'Knitcraft Cream Be My Baby DK Yarn 100g',
     category: 'Yarn',
-    brand: 'Macrametr',
-    description: '',
-    price: 1,
-    stock: 1,
-    photo: '',
+    brand: 'Knitcraft',
+    description: `
+    Ball Weight
+      100 Grams
+    Blend
+      50% Acrylic, 50% Polyamide
+    Craft
+      Knitting, Crochet
+    Crochet Hook Size
+      4.00mm (F)
+    Needle Size
+      4.00mm (US 6)
+    Tension
+      22 stitches, 28 rows to 10cm/4in
+    Collection
+      Knitcraft Be My Baby
+    Yarn Length
+      235m
+    Yarn Weight
+      DK`,
+    price: 3.30,
+    stock: 6874,
+    photo: './assets/4.jpg',
   },
   {
-    name: 'Item 5',
+    name: 'Knitcraft Mint Green Everyday DK Yarn 50g',
     category: 'Yarn',
-    brand: 'Macrametr',
-    description: '',
-    price: 1,
-    stock: 1,
-    photo: '',
+    brand: 'Knitcraft',
+    description: `
+    Ball Weight
+      50 Grams
+    Blend
+      100% Acrylic
+    Craft
+      Knitting
+    Crochet Hook Size
+      4.00mm (F)
+    Needle Size
+      4.00mm (US 6)
+    Tension
+      22 stitches, 30 rows to 10cm/4in
+    Collection
+      Knitcraft Everyday DK
+    Yarn Length
+      137m
+    Yarn Weight
+      DK`,
+    price: 1.50,
+    stock: 675,
+    photo: './assets/5.jpg',
   },
   {
-    name: 'Item 6',
+    name: 'Knitcraft Paprika Hug It Out Yarn 200g',
     category: 'Yarn',
-    brand: 'Macrametr',
-    description: '',
-    price: 1,
-    stock: 1,
-    photo: '',
+    brand: 'Knitcraft',
+    description: `
+    Ball Weight
+      200 Grams
+    Blend
+      100% Acrylic
+    Craft
+      Knitting
+    Crochet Hook Size
+      12.00mm (O)
+    Needle Size
+      12.00mm (US 17)
+    Tension
+      7 stitches, 9 rows to 10cm/4in
+    Collection
+      Knitcraft Hug It Out
+    Knitting Needle Width
+      12.00mm
+    Yarn Length
+      58m
+    Yarn Weight
+      Super Chunky`,
+    price: 7.00,
+    stock: 49,
+    photo: './assets/6.jpg',
   },
   {
-    name: 'Item 7',
+    name: 'Pony Knitting Needles 10mm x 40cm',
+    category: 'Knitting Needles',
+    brand: 'Pony',
+    description: `
+    Needle Material
+      Plastic
+    Needle Type
+      Single Point
+    Knitting Needle Width
+      10.00mm `,
+    price: 4.30,
+    stock: 9568,
+    photo: './assets/7.jpg',
+  },
+  {
+    name: 'Pony Bamboo Needles 33cm 6.00mm',
+    category: 'Knitting Needles',
+    brand: 'Pony',
+    description: `
+    Needle Material
+      Bamboo
+    Needle Type
+      Single Point
+    Knitting Needle Width
+      6.00mm`,
+    price: 6.00,
+    stock: 68,
+    photo: './assets/8.jpg',
+  },
+  {
+    name: 'Knitcraft Purple Knitting Needles 5mm',
+    category: 'Knitting Needles',
+    brand: 'Knitcraft',
+    description: `
+    Needle Material
+      Aluminium
+    Needle Type
+      Single Point
+    Knitting Needle Width
+      5.00mm`,
+    price: 3.80,
+    stock: 168,
+    photo: './assets/9.jpg',
+  },
+  {
+    name: 'Knitcraft Green Knitting Needles 8mm',
+    category: 'Knitting Needles',
+    brand: 'Knitcraft',
+    description: `
+    Needle Material
+      Aluminium
+    Needle Type
+      Single Point
+    Knitting Needle Width
+      8.00mm`,
+    price: 3.80,
+    stock: 1598,
+    photo: './assets/10.jpg',
+  },
+  {
+    name: 'Pony Flair Circular Knitting Needles 80cm 10mm',
+    category: 'Knitting Needles',
+    brand: 'Pony',
+    description: `
+    Needle Material
+      Wood
+    Needle Type
+      Circular
+    Knitting Needle Width
+      10.00mm`,
+    price: 22.00,
+    stock: 951,
+    photo: './assets/11.jpg',
+  },
+  {
+    name: 'Knitcraft Wooden Knitting Needles 15mm x 40cm',
+    category: 'Knitting Needles',
+    brand: 'Knitcraft',
+    description: `
+    Needle Material
+      Wood
+    Needle Type
+      Single Point
+    Knitting Needle Width
+      15.00mm`,
+    price: 8.00,
+    stock: 2369,
+    photo: './assets/12.jpg',
+  },
+  {
+    name: 'Hayfield Indigo Bonus Chunky Tweed Yarn 100g',
     category: 'Yarn',
-    brand: 'Macrametr',
-    description: '',
-    price: 1,
-    stock: 1,
-    photo: '',
+    brand: 'Hayfield',
+    description: `
+    Ball Weight
+      100 Grams
+    Blend
+      100% Acrylic
+    Craft
+      Knitting
+    Needle Size
+      UK 3 - 6.50mm (US 10.5)
+    Tension
+      14 Stitches, 19 Rows, 10cm/4in
+    Collection
+      Sirdar Bonus Chunky Tweed
+    Knitting Needle Width
+      6.50mm
+    Yarn Length
+      137
+    Yarn Weight
+      Chunky`,
+    price: 3.00,
+    stock: 566,
+    photo: './assets/13.jpg',
   },
   {
-    name: 'Item 8',
+    name: 'Hayfield Grape Bonus Chunky Yarn 100g',
     category: 'Yarn',
-    brand: 'Macrametr',
-    description: '',
-    price: 1,
-    stock: 1,
-    photo: '',
+    brand: 'Hayfield',
+    description: `
+    Ball Weight
+      100 Grams
+    Blend
+      100% Acrylic
+    Craft
+      Knitting, Crochet
+    Crochet Hook Size
+      6.50mm (K)
+    Needle Size
+      6.50mm (US 10.5)
+    Tension
+      14 stitches;19 rows to 10cm/4in
+    Collection
+      Hayfield Bonus
+    Yarn Length
+      137m`,
+    price: 2.50,
+    stock: 1452,
+    photo: './assets/14.jpg',
   },
   {
-    name: 'Item 9',
-    category: 'Beads',
-    brand: 'Gamma',
-    description: '',
-    price: 2,
-    stock: 1,
-    photo: '',
+    name: 'Peter Pan Fawn Precious Chunky 50g',
+    category: 'Yarn',
+    brand: 'Peter Pan',
+    description: `
+    Ball Weight
+      50 Grams
+    Blend
+      80% Nylon, 20% Polyester
+    Craft
+      Knitting
+    Crochet Hook Size
+      6.50mm (K)
+    Needle Size
+      6.50mm (US 10.5)
+    Tension
+      10 stitches to 14 rows to 10cm/4in
+    Collection
+      Peter Pan Precious Chunky
+    Yarn Length
+      75m
+    Yarn Weight
+      Chunky`,
+    price: 3.00,
+    stock: 528,
+    photo: './assets/15.jpg',
   },
   {
-    name: 'Item 10',
-    category: 'Beads',
-    brand: 'Gamma',
-    description: '',
-    price: 2,
-    stock: 1,
-    photo: '',
+    name: 'Peter Pan Angel Precious Chunky 50g',
+    category: 'Yarn',
+    brand: 'Peter Pan',
+    description: `
+    Ball Weight
+      50 Grams
+    Blend
+      80% Nylon, 20% Polyester
+    Craft
+      Knitting
+    Crochet Hook Size
+      6.50mm (K)
+    Needle Size
+      6.50mm (US 10.5)
+    Tension
+      10 stitches to 14 rows to 10cm/4in
+    Collection
+      Peter Pan Precious Chunky
+    Yarn Length
+      75m
+    Yarn Weight
+      Chunky`,
+    price: 3.00,
+    stock: 5487,
+    photo: './assets/16.jpg',
   },
   {
-    name: 'Item 11',
-    category: 'Beads',
-    brand: 'Gamma',
-    description: '',
-    price: 2,
-    stock: 1,
-    photo: '',
+    name: 'James C Brett Pastel Multi Baby Twinkle DK 100g',
+    category: 'Yarn',
+    brand: 'James C Brett',
+    description: `
+    Ball Weight
+      100 Grams
+    Blend
+      66% Acrylic, 29% Polyamide, 5% Polyester
+    Craft
+      Knitting
+    Needle Size
+      3.25mm (US 3) - 4.00mm (US 6)
+    Tension
+      22 stitches, 28 rows to 10cm/4in
+    Collection
+      James C Brett Baby DK
+    Yarn Length
+      270
+    Yarn Weight
+      DK`,
+    price: 4.30,
+    stock: 57,
+    photo: './assets/17.jpg',
   },
   {
-    name: 'Item 12',
-    category: 'Beads',
-    brand: 'Gamma',
-    description: '',
-    price: 2,
-    stock: 1,
-    photo: '',
+    name: 'James C Brett Grey Spot Dotti DK Yarn 50g',
+    category: 'Yarn',
+    brand: 'James C Brett',
+    description: `
+    Ball Weight
+      50 Grams
+    Blend
+      50% Cotton, 50% Acrylic
+    Craft
+      Knitting
+    Needle Size
+      3.25mm (US 3) - 4.00mm (US 6)
+    Tension
+      22 stitches, 28 rows to 10cm/4in
+    Collection
+      James C Brett Dotti
+    Yarn Length
+      95
+    Yarn Weight
+      DK`,
+    price: 2.50,
+    stock: 658,
+    photo: './assets/18.jpg',
   },
   {
-    name: 'Item 13',
-    category: 'Beads',
-    brand: 'Gamma',
-    description: '',
-    price: 2,
-    stock: 1,
-    photo: '',
+    name: 'James C Brett Aqua Red Fairground DK Yarn 100g',
+    category: 'Yarn',
+    brand: 'James C Brett',
+    description: `
+    Ball Weight
+      100 Grams
+    Blend
+      100% Acrylic
+    Craft
+      Knitting, Crochet
+    Crochet Hook Size
+      4.00mm (F)
+    Needle Size
+      4.00mm (US 6)
+    Tension
+      22 stitches, 28 rows to 10cm/4in
+    Collection
+      James C Brett Fairground DK
+    Knitting Needle Width
+      4.00mm
+    Yarn Length
+      232m
+    Yarn Weight
+      DK`,
+    price: 3.80,
+    stock: 4578,
+    photo: './assets/19.jpg',
   },
   {
-    name: 'Item 14',
-    category: 'Beads',
-    brand: 'Gamma',
-    description: '',
-    price: 2,
-    stock: 1,
-    photo: '',
+    name: 'James C Brett Rust Blue Marble Chunky Yarn 200g',
+    category: 'Yarn',
+    brand: 'James C Brett',
+    description: `
+    Ball Weight
+      100 Grams
+    Blend
+      100% Acrylic
+    Craft
+      Knitting
+    Needle Size
+      6.00mm (US 10), 6.00mm (US 10)
+    Tension
+      14 stitches, 20 rows to 10cm/4in
+    Collection
+      James C Brett Marble Chunky
+    Knitting Needle Width
+      6.00mm
+    Yarn Length
+      312
+    Yarn Weight
+      Chunky`,
+    price: 6.80,
+    stock: 785,
+    photo: './assets/20.jpg',
   },
   {
-    name: 'Item 15',
-    category: 'Beads',
-    brand: 'Gamma',
-    description: '',
-    price: 2,
-    stock: 1,
-    photo: '',
+    name: 'Hobbycraft Midi Sewing Machine',
+    category: 'Sewing Machines',
+    brand: 'Hobbycraft',
+    description: `
+    Machine size
+      28 x 28 x 12 cm (11 x 11 x 4.7 inches)
+    Machine weight
+      1.9kg
+    Bobbin Type
+      Drop In
+    Sewing Machine Buttonhole Stitch Feature
+      4 Step Manual
+    Sewing Machine Type
+      Mechanical
+    Number of Stitches
+      12`,
+    price: 45.00,
+    stock: 98,
+    photo: './assets/21.jpg',
   },
   {
-    name: 'Item 16',
-    category: 'Beads',
-    brand: 'Gamma',
-    description: '',
-    price: 2,
-    stock: 1,
-    photo: '',
+    name: 'Janome HC8100 Computerised Sewing Machine',
+    category: 'Sewing Machines',
+    brand: 'Janome',
+    description: `
+    Machine size
+      41 x 28 x 18cm (16.14 x 11 x 7 inches)
+    Machine weight
+      5.6kg
+    Bobbin Type
+      Drop In
+    Sewing Machine Buttonhole Stitch Feature
+      1 Step Automatic
+    Sewing Machine Type
+      Computerised
+    Number of Stitches
+      100`,
+    price: 329.00,
+    stock: 56,
+    photo: './assets/22.jpg',
   },
   {
-    name: 'Item 17',
-    category: 'Paint',
-    brand: 'Preciosa',
-    description: '',
-    price: 1,
-    stock: 1,
-    photo: '',
+    name: 'Grey Star Sewing Box',
+    category: 'Sewing Boxes',
+    brand: 'Hobbycraft',
+    description: `
+    Size
+      15 x 26 x 19cm (15 x 10.2 x 7.5 inches)
+    Includes comfortable corded handle
+    Fabric exterior with springy top!
+    Lightweight and simple storage solution
+    Perfect for storing all of your sewing essentials!`,
+    price: 19.00,
+    stock: 548,
+    photo: './assets/23.jpg',
   },
   {
-    name: 'Item 18',
-    category: 'Paint',
-    brand: 'Preciosa',
-    description: '',
-    price: 1,
-    stock: 1,
-    photo: '',
+    name: 'Forest Friends Medium Sewing Box',
+    category: 'Sewing Boxes',
+    brand: 'Hobbycraft',
+    description: `
+    Size
+      19 x 25.5 x 15cm (7.5 x 10 x 5.9 inches)
+    Includes comfortable corded handle
+    Fabric exterior with springy top!
+    Lightweight and simple storage solution
+    Perfect for storing all of your sewing essentials!`,
+    price: 24.00,
+    stock: 412,
+    photo: './assets/24.jpg',
   },
   {
-    name: 'Item 19',
-    category: 'Paint',
-    brand: 'Preciosa',
-    description: '',
-    price: 1,
-    stock: 1,
-    photo: '',
+    name: 'Springtime Knitting Bag',
+    category: 'Knitting Bags',
+    brand: 'Hobbycraft',
+    description: `
+    Size
+      15 x 45 x 17cm (5.9 x 17.7 x 6.7 inches)
+    Zip fastening
+    Includes two compartments
+    Features organised storage for your knitting needles
+    Perfect for storage knitting essentials between uses`,
+    price: 15.00,
+    stock: 783,
+    photo: './assets/25.jpg',
   },
   {
-    name: 'Item 20',
-    category: 'Paint',
-    brand: 'Preciosa',
-    description: '',
-    price: 1,
-    stock: 1,
-    photo: '',
+    name: 'Beautiful Bloom Knitting Bag',
+    category: 'Knitting Bags',
+    brand: 'Hobbycraft',
+    description: `
+    Size
+      31 x 36 x 13cm (12.2 x 14.2 x 5.1 inches)
+    Made from glossy PVC coated cotton fabric. Wipe clean
+    Sturdy, long-lasting design
+    Features a range of different pockets for storing various items
+    Great for crafters on the move`,
+    price: 20.00,
+    stock: 1236,
+    photo: './assets/26.jpg',
   },
   {
-    name: 'Item 21',
-    category: 'Paint',
-    brand: 'Preciosa',
-    description: '',
-    price: 1,
-    stock: 1,
-    photo: '',
+    name: 'Mini Polar Bear Needle Felting Kit',
+    category: 'Needle Felting Kits',
+    brand: 'Trimits',
+    description: `
+    Finished size approx. 8.5 x 10.8cm (3.3 x 4.3 inches)
+    Includes instructions, a felting needle, roving wool, polystyrene shapes, felt pieces and jute string
+    20% wool, 80% polyester/polystyrene
+    Do not wash
+    Great for introducing yourself to needle felting
+    Perfect for crafting a felted Christmas decoration`,
+    price: 6.00,
+    stock: 2569,
+    photo: './assets/27.jpg',
   },
   {
-    name: 'Item 22',
-    category: 'Paint',
-    brand: 'Preciosa',
-    description: '',
-    price: 1,
-    stock: 1,
-    photo: '',
+    name: 'Giraffe and Lion Needle Felting Kit',
+    category: 'Needle Felting Kits',
+    brand: 'Hobbycraft',
+    description: `
+    Kit Includes Instructions, 2 x felt shapes, 2 x felting needles, 1 polystyrene shapes 1, 1 x polystyrene shape 2, brown, black and cream felt
+    Glue not included
+    Use the pictures for your inspiration and guidance
+    Create your very own charming decorations`,
+    price: 4.50,
+    stock: 3654,
+    photo: './assets/28.jpg',
   },
   {
-    name: 'Item 23',
-    category: 'Paint',
-    brand: 'Preciosa',
-    description: '',
-    price: 1,
-    stock: 1,
-    photo: '',
+    name: 'Mini Penguins Needle Felting Kit',
+    category: 'Needle Felting Kits',
+    brand: 'Trimits',
+    description: `
+    Finished size approx. 9.5 x 6.5cm (3.7 x 2.6 inches)
+    Includes instructions, a felting needle, roving wool, polystyrene shapes, felt pieces and jute string
+    20% wool, 80% polyester/polystyrene
+    Do not wash
+    Great for introducing yourself to needle felting
+    Perfect for crafting a felted Christmas decoration`,
+    price: 4.00,
+    stock: 541,
+    photo: './assets/29.jpg',
   },
   {
-    name: 'Item 24',
-    category: 'Paint',
-    brand: 'Preciosa',
-    description: '',
-    price: 1,
-    stock: 1,
-    photo: '',
-  },
-  {
-    name: 'Item 25',
-    category: 'Colored Pencils',
-    brand: 'Derwent',
-    description: '',
-    price: 2,
-    stock: 1,
-    photo: '',
-  },
-  {
-    name: 'Item 26',
-    category: 'Colored Pencils',
-    brand: 'Derwent',
-    description: '',
-    price: 2,
-    stock: 1,
-    photo: '',
-  },
-  {
-    name: 'Item 27',
-    category: 'Colored Pencils',
-    brand: 'Derwent',
-    description: '',
-    price: 2,
-    stock: 1,
-    photo: '',
-  },
-  {
-    name: 'Item 28',
-    category: 'Colored Pencils',
-    brand: 'Derwent',
-    description: '',
-    price: 2,
-    stock: 1,
-    photo: '',
-  },
-  {
-    name: 'Item 29',
-    category: 'Colored Pencils',
-    brand: 'Derwent',
-    description: '',
-    price: 2,
-    stock: 1,
-    photo: '',
-  },
-  {
-    name: 'Item 30',
-    category: 'Colored Pencils',
-    brand: 'Derwent',
-    description: '',
-    price: 2,
-    stock: 1,
-    photo: '',
-  },
-  {
-    name: 'Item 31',
-    category: 'Colored Pencils',
-    brand: 'Derwent',
-    description: '',
-    price: 2,
-    stock: 1,
-    photo: '',
-  },
-  {
-    name: 'Item 32',
-    category: 'Colored Pencils',
-    brand: 'Derwent',
-    description: '',
-    price: 2,
-    stock: 1,
-    photo: '',
+    name: 'Cat Needle Felting Kit',
+    category: 'Needle Felting Kits',
+    brand: 'Hobbycraft',
+    description: `
+    Instructions, thread, 1 x polystyrene ball, 2 x felting needles, 1 x polystyrene egg, 2 x black beads (eyes), cream, black, light brown and pink felt
+    Glue not included
+    Use the pictures for your inspiration and guidance
+    Create your very own charming decorations`,
+    price: 4.50,
+    stock: 958,
+    photo: './assets/30.jpg',
   },
 ]
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,39 @@
-import './style.scss';
-import './assets/yarn-macrame.png';
+// styles import
 
+import './style.scss';
+
+// assets import
+
+import './assets/yarn-macrame.png';
+import './assets/1.jpg';
+import './assets/2.jpg';
+import './assets/3.jpg';
+import './assets/4.jpg';
+import './assets/5.jpg';
+import './assets/6.jpg';
+import './assets/7.jpg';
+import './assets/8.jpg';
+import './assets/9.jpg';
+import './assets/10.jpg';
+import './assets/11.jpg';
+import './assets/12.jpg';
+import './assets/13.jpg';
+import './assets/14.jpg';
+import './assets/15.jpg';
+import './assets/16.jpg';
+import './assets/17.jpg';
+import './assets/18.jpg';
+import './assets/19.jpg';
+import './assets/20.jpg';
+import './assets/21.jpg';
+import './assets/22.jpg';
+import './assets/23.jpg';
+import './assets/24.jpg';
+import './assets/25.jpg';
+import './assets/26.jpg';
+import './assets/27.jpg';
+import './assets/28.jpg';
+import './assets/29.jpg';
+import './assets/30.jpg';
 
 console.log('Hello, World!');
