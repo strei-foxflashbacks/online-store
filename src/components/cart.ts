f4ec318dc55d4c0aa0711de953ae9c2f67a4c9ff
Inna T <meeoow005@gmail.com>
2022-12-21 15:44:45 +0100

    refactor: add comments, add variables

--- src/components/cart.ts
+++ src/components/cart.ts
@@ -1,11 +1,11 @@
 import { IProductData } from "../types/IProductData";
 
 export class Cart {
-  private products: Array<IProductData>;
+  private products: Array<IProductData>;//переделать в { <IProductData> : countInCart }
   constructor() {
     this.products = [];
   }
-  countOfProducts() {
+  countOfProducts() {//уточнить корректно ли считать повторяющиеся продукты
     return this.products.length;
   }
   getSumOfProducts() {
@@ -14,6 +14,14 @@ export class Cart {
     return sumOfProducts;
   }
   addProductToCart(product: IProductData) {
+    let currentProductName: string = product.name;
+    let countThisProducts: number = 1;
+    let productCounter = { currentProductName : countThisProducts};
+    // if (this.products.includes(product.name)) {
+    //
+    // }//переделать массив продуктов на массив ключ-продукт - значение-количество на складе
+    //добавить сравнение значения (количества продуктов) с остатком на складе
+
     this.products.push(product);
   }
   deleteProduct(product: IProductData) {
