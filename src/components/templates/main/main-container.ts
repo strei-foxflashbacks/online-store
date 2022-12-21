58832d0039a5fef9bb6a49e9ccc5321102234443
ssekridov@gmail.com <ssekridov@gmail.com>
2022-12-21 14:03:15 +0100

    fix: remove unneeded comments

--- src/components/templates/main/main-container.ts
+++ src/components/templates/main/main-container.ts
@@ -1,6 +1,5 @@
 import getFilters from "./filters";
 import getCatalog from "./catalog";
-// import setCatalog from "../../setCatalog";
 
 const getMainContainer = (): HTMLElement => {
   const mainContainer = document.createElement('div');
@@ -8,8 +7,6 @@ const getMainContainer = (): HTMLElement => {
   mainContainer.className = 'main-container';
 
   mainContainer.append(getFilters(), getCatalog())
-
-  // setCatalog()
 
   return mainContainer;
 }
