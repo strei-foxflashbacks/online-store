7dea48a8c3ea309137199fa0f29f850f9ff97a28
Inna T <meeoow005@gmail.com>
2022-12-28 17:19:11 +0100

    feat: move cart button to bottom on the page

--- src/components/templates/header/content.ts
+++ src/components/templates/header/content.ts
@@ -7,7 +7,7 @@ const getContent = (): HTMLElement => {
   <h1 class="logo">Hobby Art</h1>
   <div class="header-elements">
     <div class="cart-container"></div>
-    <button type="button" class="button">Cart</button>
+    <button type="button" class="button cart-button">Cart</button>
     <span class="cart-counter" id="cartCounter">0</span>
   </div>`
 
