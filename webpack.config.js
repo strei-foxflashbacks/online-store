6cf0533d5177cb61095be505604cc25d017594d4
ssekridov@gmail.com <ssekridov@gmail.com>
2022-12-29 15:33:41 +0100

    feat: router public path

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,10 @@ const baseConfig = {
         filename: 'index.js',
         path: path.resolve(__dirname, './dist'),
         assetModuleFilename: 'assets/[name][ext]',
+        publicPath: '/',
+    },
+    devServer: {
+        historyApiFallback: true,
     },
     plugins: [
         new HtmlWebpackPlugin({
