3ce2ed5ac2ff2d07e5cb26ae79acc31419c72dcf
Inna T <meeoow005@gmail.com>
2023-01-02 01:55:56 +0100

    feat: add breadcrumbs to product page

--- src/components/productPage.ts
+++ src/components/productPage.ts
@@ -2,6 +2,8 @@ import clearPage from "./clearPage"
 import setDefaultPage from "./defaultPage";
 import getProductCard from "./templates/main/product-card";
 import { productData } from "./product-data";
+import './router/routerFuncs'
+import {getMainNavigate, goBack} from "./router/routerFuncs";
 
 const getProductPage = (id: number) => {
   window.scroll(0, 0)
@@ -13,6 +15,8 @@ const getProductPage = (id: number) => {
   const main = document.querySelector('main') as HTMLElement;
 
   main.append(getProductCard(product))
-  // console.log(product)
+
+  goBack();
+  getMainNavigate();
 }
 export default getProductPage
