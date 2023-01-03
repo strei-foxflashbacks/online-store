a9627697f1e011669912661f2a4cc6eed4b2fa0c
Inna T <meeoow005@gmail.com>
2023-01-03 17:49:05 +0100

    fix: delete console.logs, eventListener inside eventListener

--- src/components/functions/cart_functions/handleOrderButton.ts
+++ src/components/functions/cart_functions/handleOrderButton.ts
@@ -3,9 +3,7 @@ export function handleOrderButton() {
   console.log('handle button');
 
   const modal = document.querySelector('.modal-pay') as HTMLElement;
-  console.log(modal);
   const darkBackground = document.querySelector('#darkBackground') as HTMLElement;
-  console.log(darkBackground)
   if (modal && darkBackground) {
     modal.style.visibility = 'visible';
     darkBackground.style.visibility = 'visible';
