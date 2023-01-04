321bb9cf6cebf416fa9194879fd03d6038abe174
ssekridov@gmail.com <ssekridov@gmail.com>
2023-01-05 00:40:03 +0100

    feat: mixed sorting

--- src/components/functions/filters/brandFilter.ts
+++ src/components/functions/filters/brandFilter.ts
@@ -4,25 +4,29 @@ import { productData } from "../../product-data";
 const brandFilter = () => {
   const filterableData: IProductData[] = productData;
   const productCatalog = document.querySelector('.catalog') as HTMLElement;
-  const catalog = document.querySelectorAll('.catalog-card');
-  const catalogQueue = Array.from(catalog);
+  const fullCatalog = document.querySelectorAll('.catalog-card');
   const output: Element[] = [];
+  let capturedOutput: Element[] = [];
+  let capturedLength = 0;
 
   const brands: NodeListOf<HTMLInputElement> = document.querySelectorAll('.brand-filter');
 
   brands.forEach((brand: HTMLInputElement) => {
     brand.addEventListener('change', () => {
-
-      const brandFiltered = filterableData.filter(element => element.brand === brand.name)
-      const filteredIds = brandFiltered.map(element => `${element.id}`)
+      const brandFiltered = filterableData.filter(element => element.brand === brand.name);
+      const filteredIds = brandFiltered.map(element => `${element.id}`);
+      const catalog = document.querySelectorAll('.catalog-card');
+      const catalogQueue = Array.from(catalog);
 
       while (productCatalog.firstChild) {
-        productCatalog.removeChild(productCatalog.firstChild)
+        productCatalog.removeChild(productCatalog.firstChild);
       }
 
       if (brand.checked === true) {
         catalogQueue.forEach(element => {
           if (filteredIds.includes(element.id)) {
+            capturedOutput = catalogQueue;
+            capturedLength = catalogQueue.length
             output.push(element)
             output.forEach(node => productCatalog.append(node))
           }
@@ -36,9 +40,12 @@ const brandFilter = () => {
             output.forEach(node => productCatalog.append(node))
           }
         })
-
-        if (productCatalog.childNodes.length === 0) {
-          catalogQueue.forEach(node => {
+        if (capturedLength === 30) {
+          fullCatalog.forEach(node => {
+            productCatalog.append(node)
+          })
+        } else {
+          capturedOutput.forEach(node => {
             productCatalog.append(node)
           })
         }
--- src/components/functions/filters/categoryFilter.ts
+++ src/components/functions/filters/categoryFilter.ts
@@ -4,25 +4,29 @@ import { productData } from "../../product-data";
 const categoryFilter = () => {
   const filterableData: IProductData[] = productData;
   const productCatalog = document.querySelector('.catalog') as HTMLElement;
-  const catalog = document.querySelectorAll('.catalog-card');
-  const catalogQueue = Array.from(catalog);
+  const fullCatalog = document.querySelectorAll('.catalog-card');
   const output: Element[] = [];
+  let capturedOutput: Element[] = []
+  let capturedLength = 0;
 
   const categories: NodeListOf<HTMLInputElement> = document.querySelectorAll('.category-filter');
 
   categories.forEach((category: HTMLInputElement) => {
     category.addEventListener('change', () => {
-
-      const categoryFiltered = filterableData.filter(element => element.category === category.name)
-      const filteredIds = categoryFiltered.map(element => `${element.id}`)
+      const categoryFiltered = filterableData.filter(element => element.category === category.name);
+      const filteredIds = categoryFiltered.map(element => `${element.id}`);
+      const catalog = document.querySelectorAll('.catalog-card');
+      const catalogQueue = Array.from(catalog);
 
       while (productCatalog.firstChild) {
-        productCatalog.removeChild(productCatalog.firstChild)
+        productCatalog.removeChild(productCatalog.firstChild);
       }
 
       if (category.checked === true) {
         catalogQueue.forEach(element => {
           if (filteredIds.includes(element.id)) {
+            capturedOutput = catalogQueue;
+            capturedLength = catalogQueue.length;
             output.push(element)
             output.forEach(node => productCatalog.append(node))
           }
@@ -36,9 +40,12 @@ const categoryFilter = () => {
             output.forEach(node => productCatalog.append(node))
           }
         })
-
-        if (productCatalog.childNodes.length === 0) {
-          catalogQueue.forEach(node => {
+        if (capturedLength === 30) {
+          fullCatalog.forEach(node => {
+            productCatalog.append(node)
+          })
+        } else {
+          capturedOutput.forEach(node => {
             productCatalog.append(node)
           })
         }
