646c0894a136f2c507ee882767a5298608082115
Inna T <meeoow005@gmail.com>
2023-01-05 20:04:05 +0100

    feat: add handler for plus (cart page)

--- src/components/functions/addProductToCart.ts
+++ src/components/functions/addProductToCart.ts
@@ -53,3 +53,73 @@ function deleteProductFromCart(cart: ICartProductRecord[], idProduct: number) {
   }
 }
 
+
+//функции для работы с продуктом из страницы корзины
+function getProductFromEvent(event: Event) {
+  //нахожу айди продукта
+  const button = event.target as HTMLElement;
+  if (!button) {
+    throw new Error('button is not found');
+  }
+  if (!(button.parentElement) || !(button.parentElement.parentElement)) {
+    throw new Error('card is not found');
+  }
+  const product = button.parentElement.parentElement;
+  console.log(product);
+  if (!product.id) {
+    throw new Error('id card is not found');
+  }
+  return product;
+}
+
+export function increaseHandler(event: Event) {
+  const cart = localStorage.getItem('cart');
+  if (!cart) {
+    throw new Error('Cart is gone!');
+  }
+  const product = getProductFromEvent(event);
+  const idCard = product.id;
+  const stock = Number(product.getAttribute('data-count'));
+  if (!stock) {
+    throw new Error('stock is not found');
+  }
+
+  //нахожу counter для изменения
+  let counter: HTMLElement | null = null;
+  const productElements = product.children;
+  for (const child of productElements) {
+    if (child.classList.contains('counter-number')) {
+      counter = child as HTMLElement;
+      break;
+    }
+  }
+  if (!counter) {
+    throw new Error('counter is not found!');
+  }
+
+  //нажатый элемент ищется в localStorage и увеличивается его количество
+  let cartProductRecord: ICartProductRecord;
+  const objCart = JSON.parse(cart);
+  for (let i = 0; i < objCart.length; i++) {
+    if (objCart[i].id == idCard) {
+      cartProductRecord = objCart[i];
+      if (stock > cartProductRecord.count) {
+        cartProductRecord.count = cartProductRecord.count + 1;
+        counter.innerText = `${cartProductRecord.count}`;
+      }
+      localStorage.setItem('cart', JSON.stringify(objCart));
+      break;
+    }
+  }
+}
+
+
+
+// export function decreaseHandler() {
+//
+// }
+//
+// export function removeHandler() {
+//
+// }
+
