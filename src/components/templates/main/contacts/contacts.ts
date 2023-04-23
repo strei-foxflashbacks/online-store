cf713636e5a0ab202fac038385f3bfea588e63b5
Inna T <meeoow005@gmail.com>
2023-04-24 01:38:43 +0200

    fix: icons color

--- src/components/templates/main/contacts/contacts.ts
+++ src/components/templates/main/contacts/contacts.ts
@@ -4,20 +4,20 @@ const getContacts = (): HTMLElement => {
   contactsContainer.className = 'container-contacts';
 
   contactsContainer.innerHTML = `
-  <div class="contacts">
-    <div class="contacts__item"><img src="../../assets/phone.svg" alt="phone icon" class="icon">
-      <div class="contacts__text">+382 020 000 000</div>
+  <div class="contacts" xmlns="http://www.w3.org/1999/html">
+    <div class="contacts__item"><div class="icon" id="phone"></div>
+      <div class="contacts__text"><a href="tel:+382020000000">+382 020 000 000</a></div>
     </div>
-    <div class="contacts__item"><img src="../../assets/map.svg" alt="map icon" class="icon">
-      <div>Podgorica, Studentska bb,
-        lamela 11
+    <div class="contacts__item">
+    <div class="icon" id="map"></div>
+      <a href="https://goo.gl/maps/Qsc1V7dD275uLDYn8">Podgorica, Studentska bb,
+        lamela 11</a>
       </div>
-    </div>
-    <div class="contacts__item"><img src="../../assets/clock.svg" alt="clock icon" class="icon">
+    <div class="contacts__item"><div class="icon" id="clock"></div>
       <div>Working clock: 08:00 -
         17:00
       </div>
-    </div>
+    </img>
   </div>`
 
   return contactsContainer;
